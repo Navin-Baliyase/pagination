45f2c07a52f1677536ad1345ccbf3258b8b3f106
Navin-Baliyase <navinkumar2508@gmail.com>
2018-10-02 08:35:27 +0530

    added pagination gem and listed in index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles=[
+           ["title1","content1"],
+		   ["title2","content2"],
+		   ["title3","content3"],
+		   ["title4","content4"],
+		   ["title5","content5"],
+		   ["title6","content6"],
+		   ["title7","content7"],
+		   ["title8","content8"],
+		   ["title9","content9"],
+		   ["title10","content11"],
+		   ["title12","content12"],
+		   ["title13","content13"],
+		   ["title14","content14"],
+		   ["title15","content15"],
+		   ["title16","content16"],
+		 ]
+		 
+articles.each do |title,content|
+Article.create(title:title,content:content)
+end
